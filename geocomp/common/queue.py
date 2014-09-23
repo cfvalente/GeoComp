9735da7b798ff01b36143e301383487db32b5a42
cfvalente <caio99valente@hotmail.com>
2014-09-23 02:58:30 -0300

    pilha

---
+++ geocomp/common/queue.py
@@ -0,0 +1,37 @@
+class qnode():
+
+
+	def __init__(self, value):
+		self.value = value
+		self.next = None
+		self.previous = None
+
+
+class queue():
+
+	def __init__(self):
+		self.top = None
+		self.size = 0
+
+	def insert(self, value):
+		node = qnode(value)
+		if(self.size == 0):
+			self.top = node
+		else:
+			self.top.next = node
+			node.previous = self.top
+			self.top = node
+		self.size = self.size+1
+
+	def remove(self):
+		if(self.size > 0):
+			self.size = self.size-1
+			node = self.top 
+			self.top = self.top.previous
+			return node.value
+		return None
+
+	def getTop(self):
+		if(self.size > 0):
+			return self.top.value
+		return None
